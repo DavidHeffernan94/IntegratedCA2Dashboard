8bb704a26fdaa96ebd8671b3e7a328946c9d4151
DavidHeffernan94 <sba24313@student.cct.ie>
2026-05-13 12:12:23 +0100

    df

--- Dashboard.py
+++ Dashboard.py
@@ -2,5 +2,15 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title("Dashboard")
-st.write("Hello")
+@st.cache_data  
+def load_data():
+    data = pd.read_csv("OnlineRetail_Cleaned.csv")  
+    data["Date"] = pd.to_datetime(data["Date"])      
+    return data 
+    
+
+df = load_data()
+
+if st.checkbox("show raw data"):
+    st.subheader("Raw Data")
+    st.write(data)
